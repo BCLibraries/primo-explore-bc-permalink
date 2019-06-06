cb198b1591d7a0c379ff2ffc9fca0b1f4cc0981c
Luke Gaudreau <gaudreal@bc.edu>
2019-06-06 14:42:21 -0400

    Update to short permalink
    Refactor to eliminate unnecessary addition of permalink-after

--- js/permalink.module.js
+++ js/permalink.module.js
@@ -6,44 +6,20 @@ angular
         vm.$onInit=function () {
             vm.permalinkText='';
             // change permalink to correct url
-            $scope.$watch('vm.parentCtrl.permalink',function () {
+            $scope.$watch('vm.parentCtrl.shortPermalink',function () {
                 if(vm.parentCtrl.item){
                     if(vm.parentCtrl.item.pnx.search.lsr13 && vm.parentCtrl.item.pnx.addata.lad09 == "DAO") {
-                        vm.parentCtrl.permalink = 'https://hdl.handle.net/' + vm.parentCtrl.item.pnx.search.lsr13[0];
+                        vm.parentCtrl.shortPermalink = 'https://hdl.handle.net/' + vm.parentCtrl.item.pnx.search.lsr13[0];
                     }
-                }
-                if(vm.parentCtrl.permalink) {
-                    vm.permalink=$sce.trustAsHtml(vm.parentCtrl.permalink);
-                    // remove parent node
-                    var pNode=$element[0].parentNode.children[0];
-
-                    if(pNode) {
-                       pNode.style.display='none';
-                    }
-                    // get link text
-                    setTimeout(()=>{
-                        var el=$element[0].children[0].children[0].children[0].children[0].children[0].children[0];
-                        if(el) {
-                            vm.permalinkText=el.textContent;
-                        }
-                    },1000);
-
                 }
             });
 	};
 
-	vm.selectText=function() {
-            vm.msg.class = 'highlite';
-        };
-        vm.unSelectText=function() {
-            vm.msg.class = '';
-        };
-
-    }])
+])
 
     .component('prmPermalinkAfter',{
         bindings:{parentCtrl:'<'},
         controller: 'prmPermalinkAfterController',
-	controllerAs: 'vm',
-        templateUrl:'primo-explore-bc-permalink/html/permalink.html'
+        controllerAs: 'vm',
+        templateUrl:''
 });
