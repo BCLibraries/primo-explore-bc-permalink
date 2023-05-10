6ab461e39a9294327053dfec7ba773daafcdb60d
Scot Colford <scot.colford@bc.edu>
2023-05-10 11:48:11 -0400

    Correct PNX section for local fields

--- js/permalink.module.js
+++ js/permalink.module.js
@@ -8,8 +8,8 @@ angular
             // change permalink to correct url
             $scope.$watch('vm.parentCtrl.shortPermalink',function () {
                 if(vm.parentCtrl.item){
-                    if(vm.parentCtrl.item.pnx.search.lsr13 && vm.parentCtrl.item.pnx.addata.lad09 == "DAO") {
-                        vm.parentCtrl.shortPermalink = 'https://hdl.handle.net/' + vm.parentCtrl.item.pnx.search.lsr13[0];
+                    if(vm.parentCtrl.item.pnx.display.lds35 && vm.parentCtrl.item.pnx.display.lds36 == "DAO") {
+                        vm.parentCtrl.shortPermalink = 'https://hdl.handle.net/' + vm.parentCtrl.item.pnx.display.lds35[0];
                     }
                 }
             });
